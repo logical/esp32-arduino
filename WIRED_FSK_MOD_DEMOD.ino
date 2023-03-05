677dc6f52b5061d86e451ec7a8f519105177d816
logical <eetekguy@gmail.com>
2023-03-05 11:31:42 -0600

    Update WIRED_FSK_MOD_DEMOD.ino
    600 baud with better math

--- WIRED_FSK_MOD_DEMOD.ino
+++ WIRED_FSK_MOD_DEMOD.ino
@@ -12,20 +12,20 @@ OLEDDisplayUi ui     ( &display );
 #define TRANSMIT_PIN 25
 #define RECEIVE_PIN 36 //adc1_0
 #define LED_BUILTIN 2
-//    Bell 202 AFSK uses a 1200 Hz tone for mark (typically a binary 1) and 2200 Hz for space (typically a binary 0).
+//    Bell 202 AFSK uses a 1200 Hz tone for mark (typically a binary 1) and 2200 Hz for space (typically a binary 0).
 #define MARK_FREQ 2200
 #define SPACE_FREQ 1200
 
-#define SAMPLE_BUFFER_SIZE 128
-#define SAMPLE_RATE 96000
+#define SAMPLE_BUFFER_SIZE 32
+#define SAMPLE_RATE 32000
 
-#define SQUELCH 100
+#define SQUELCH 300
 
-const float baud=300;
-const int bitTime =1000000*(1.0/baud);//time in us
+#define BAUD 600
+const int bitTime =1000000*(1.0/BAUD);//time in us
 
 int16_t rawSamples[SAMPLE_BUFFER_SIZE];
-int16_t sampleData[SAMPLE_BUFFER_SIZE/4];
+int16_t sampleData[SAMPLE_BUFFER_SIZE/2];
 
 
 i2s_config_t i2s_config = {
@@ -89,10 +89,17 @@ void transmitFSK(String sendMessage){
  const uint8_t markZero = SAMPLE_RATE/4/MARK_FREQ/2;
   const uint8_t spaceZero = SAMPLE_RATE/4/SPACE_FREQ/2;
 
-bool getSampleData(){
+int8_t hiI[16]={ 0   , 97,   126  ,  67   ,-40  ,-118 , -114 ,  -30 ,   75 ,  128  ,  91  , -10 , -104 , -124   ,-58 ,   49};
+int8_t hiQ[16]={128    ,83  , -20 , -109 , -122,   -49  ,  58  , 124 ,  104   , 10  , -91 , -128   ,-75   , 30  , 114  , 118};
+int8_t loI[16]={0  ,  58 ,  104 ,  126,   122  ,  91 ,   40  , -20  , -75 , -114,  -128,  -114 ,  -75 ,  -20  ,  40  ,  91};
+int8_t loQ[16]={28  , 114 ,   75,    20 ,  -40   ,-91  ,-122 , -126,  -104  , -58 ,    0  ,  58 ,  104  , 126 ,  122   , 91};
+
+int8_t readBit(){
   
   bool squelch=false;
-  double av;
+  float av;
+  //need to use floats or my accuracy is bad
+  float outloi = 0, outloq = 0, outhii = 0, outhiq = 0;
   size_t bytes = 0;
 
   
@@ -102,31 +109,61 @@ bool getSampleData(){
 
   if (result != ESP_OK){
     Serial.println("read error");
-    return false;
+    return -1;
   }
 
   // dump the samples out to the serial channel.
   if(samples_read<SAMPLE_BUFFER_SIZE){
     Serial.println("read error");
-    return false;
+    return -1;
   }
 
-  for(int i=0,j=0;i<SAMPLE_BUFFER_SIZE;i+=4){
-    sampleData[j]=((rawSamples[i] & 0x0FFF)+(rawSamples[i+1] & 0x0FFF)+
-                (rawSamples[i+2] & 0x0FFF)+(rawSamples[i+3] & 0x0FFF))/4;
+  for(int i=0,j=0;i<SAMPLE_BUFFER_SIZE;i+=2){
+    sampleData[j]=((rawSamples[i] & 0x0FFF)+(rawSamples[i+1] & 0x0FFF))/2;
     av+=sampleData[j];
     j++;
   }
   
-    av=av/(sizeof(sampleData)/sizeof(sampleData[0]));
-//    Serial.println(av);    
-    for(int i=0;i<(SAMPLE_BUFFER_SIZE/4);i++){
+    av/=SAMPLE_BUFFER_SIZE/2;
+    for(int i=0;i<(SAMPLE_BUFFER_SIZE/2);i++){
       sampleData[i]-=av;
-      if(sampleData[i] > SQUELCH)squelch=true;     
-//      Serial.println(sampleData[i]);
-    }
+//      sampleData[i]*=4;
+//    Serial.println(sampleData[i]);    
 
-  return squelch;   
+      if(sampleData[i] > SQUELCH)squelch=true;     
+
+      outloi += sampleData[i] * loI[i];
+      outloq += sampleData[i] * loQ[i];
+      outhii += sampleData[i] * hiI[i];
+      outhiq += sampleData[i] * hiQ[i];
+    }
+// Serial.println(outhii * outhii + outhiq * outhiq - outloi * outloi + outloq * outloq);
+
+int32_t magl=sqrt(outloi*outloi+outloq*outloq);
+int32_t magh=sqrt(outhii*outhii+outhiq*outhiq);
+/*
+Serial.print(outloi);
+Serial.print(" ");
+Serial.print(outloq);
+Serial.print(" ");
+Serial.print(outhii);
+Serial.print(" ");
+Serial.print(outhiq);
+Serial.print(" ");
+Serial.println(" " );
+*/
+/*
+Serial.print(magl);
+Serial.print(" ");
+Serial.print(magh);
+Serial.print(" ");
+Serial.println(" ");
+*/
+
+if(squelch==false)return -1;
+if(magl<magh)return 1;
+return 0;
+
 
 }
 
@@ -154,29 +191,8 @@ static int demodulate (int16_t data[], int idx) {
 }
 */
 
-inline int8_t readBit(){
-  uint16_t len =SAMPLE_BUFFER_SIZE/4;
-   uint16_t zeros[2]={0,len};
-   uint16_t count=0;
-   uint16_t diff=0;
-  
-  getSampleData(); 
-   
-    for(uint16_t i=1;i<len;i++){
-    if( sampleData[i-1]>0 && sampleData[i]<0  ||
-      sampleData[i-1]<0 && sampleData[i]>0)zeros[count++]=i;//read half cycles
-    if(count==2)break;        
-    }
-    diff=zeros[1]-zeros[0];       
-    if((diff <= (markZero+2))  && (diff >= (markZero-2)))return 1; //bandpass filter  
-    if((diff <= (spaceZero+2)) && (diff >= (spaceZero-2)))return 0;   
-//return 0;    
-  return -1;  
-
-}
-
-char receiveASCII(){
-    uint8_t count;
+inline char receiveASCII(){
+    uint16_t count;
 unsigned long startTime,endTime;
     uint8_t newchar=0b00000000;
 
@@ -184,28 +200,20 @@ unsigned long startTime,endTime;
   int8_t newbit=0;
   int8_t bits=0;
 
-//about 1-2 ms per read
-//     startTime=micros();
+  while(readBit()==1); //wait for stop bit to finish   
+  delayMicroseconds(bitTime*1.5);// skip 0 start bit
 
-   for(count=0;count<500;count++)if(readBit()==1)break;// wait for stop bit
-    if (count==500)return 0;
-    while(readBit()==1); //wait for stop bit to finish   
-    delayMicroseconds(bitTime*1.2);// skip 0 start bit
+  for(bits=0;bits<8;bits++){
+    startTime=micros();
+    newbit=readBit();
+//      Serial.print(newbit);
+    if(newbit==-1)newbit=readBit();//error correction
+    if(newbit==-1)return 0;
+    newchar |= (bool(newbit) << bits);
+    delayMicroseconds(bitTime-(micros()-startTime));
+  }
 
-    for(bits=0;bits<8;bits++){//7 data bits
-//      startTime=micros();
-      newbit=readBit();
-      //Serial.print(newbit);
-      if(newbit==-1)newbit=readBit();//error correction
-      if(newbit==-1)newbit=readBit();//error correction
-      if(newbit==-1)return 0;
-      newchar |= (bool(newbit) << bits);
-      delayMicroseconds(bitTime);
-    }
-//    delay(bitTime);
-//    for(uint8_t i=0;i<200;i++)if(readBit(rawSamples,RX_BUFFER_SIZE)==0)break;//zero stop bit
-      //   Serial.println();
-        return newchar;
+  return newchar;
  
 
 }
@@ -241,7 +249,8 @@ unsigned long startTime,endTime;
   String rxMessage;
   char c;
 
-  while(getSampleData()){
+
+  while(readBit()>-1){
 
     c=receiveASCII();
 
@@ -271,6 +280,11 @@ unsigned long startTime,endTime;
       txMessage+=ch;
     }    
   }
+
 }
+
+
+
+
 
 
